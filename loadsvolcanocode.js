08045d5bdfe3a71814164870629299677c0c22e9
githubuser0099 <91236558+githubuser0099@users.noreply.github.com>
2021-09-26 10:50:54 +1000

    Add files via upload
    csv file is here, but the actual volcanocode.json file points to the csv file in DataRepo, I wanted to keep my data stored separately.

---
+++ loadsvolcanocode.js
@@ -0,0 +1,5 @@
+var vg_1 = "volcanocode.json";
+
+vegaEmbed("#Dot_Map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
